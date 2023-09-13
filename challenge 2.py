dff464fc0629a688c945e531c2b683e9ce355771
K93a60 <144470682+K93a60@users.noreply.github.com>
2023-09-13 17:08:38 +0530

    Add files via upload

--- challenge 2.py
+++ challenge 2.py
@@ -1,13 +1,25 @@
-# Leap Year
+class Player:
+    def __init__(self, name):
+        self.name = name
 
-def isLeapYear(year):
-	if (year%4==0 and year%100!=0) or (year%400==0):
-		return True
-	else:
-		return False
-		
-year = int(input("Enter the year : "))
-if isLeapYear(year):
-	print("{} is leap year.".format(year))
-else:
-	print("{} is not a leap year.".format(year))
+    def play(self):
+        print(f"{self.name} is playing cricket.")
+
+class Batsman(Player):
+    def play(self):
+        print(f"The batsman {self.name} is batting.")
+
+class Bowler(Player):
+    def play(self):
+        print(f"The bowler {self.name} is bowling.")
+
+# Get player's name from the user
+player_name = input("Enter the player's name: ")
+
+# Create objects of the Batsman and Bowler classes with the player's name
+batsman = Batsman(player_name)
+bowler = Bowler(player_name)
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
